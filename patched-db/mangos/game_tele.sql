7b6ad011b35a907e338888e0b09544a210dfdd2c
Unity <Unity@localhost>
2014-07-19 18:14:17 -0400

    Add custom game_tele locations to patched-db.

--- patched-db/mangos/game_tele.sql
+++ patched-db/mangos/game_tele.sql
@@ -34,7 +34,7 @@ CREATE TABLE `game_tele` (
   `map` smallint(5) unsigned NOT NULL DEFAULT '0',
   `name` varchar(100) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1425 DEFAULT CHARSET=utf8 COMMENT='Tele Command';
+) ENGINE=MyISAM AUTO_INCREMENT=2015 DEFAULT CHARSET=utf8 COMMENT='Tele Command';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -1467,6 +1467,21 @@ INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orient
 INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (1422,8447.44,2698.09,656.783,2.8392,571,'shadowvault');
 INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (1423,8447.44,2698.09,656.783,2.8392,571,'theshadowvault');
 INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (1424,6122.16,-1061.5,402.568,4.77853,571,'k3');
+INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (2000,128.205,135.136,236.11,4.59132,37,'AzsharaCrater');
+INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (2001,-11052.9,-1568.93,27.233,4.64509,0,'Crypt');
+INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (2002,1784.23,924.563,15.581,3.5177,580,'swp');
+INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (2003,16321.5,16182,69.4439,3.00529,451,'programmerisland');
+INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (2004,16303.5,-16173.5,40.4344,6.27019,451,'designerisland');
+INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (2005,-2604.74,-2077.5,126.758,0.677952,169,'emeralddream');
+INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (2006,-11367.2,-4733.35,5.02288,3.94349,1,'northisland');
+INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (2007,883.667,-3688.95,27.0134,5.25826,1,'barrensghhut');
+INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (2008,-8833.38,628.628,94.0066,1.06535,0,'sw');
+INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (2009,-4918.88,-940.406,501.564,5.42347,0,'if');
+INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (2010,16070.9,-16686.1,78.3842,4.92128,451,'designervillage');
+INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (2011,2362.36,-5660.89,426.026,3.57071,609,'ebonhold');
+INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (2012,-1851.87,-4142.73,10.4546,4.8172,0,'forbiddingseafarm');
+INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (2013,-10735,2467,6.71548,0.996637,1,'silithusfarm');
+INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (2014,7336.09,-1540.69,161.015,0.234758,1,'dancingtrolls');
 /*!40000 ALTER TABLE `game_tele` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
